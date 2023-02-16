7fbfaaa5abdf94db877369ce459680f5962e5866
nuobasic <nuobasic@naver.com>
2023-02-16 21:22:51 +0900

    feat: post usersRepository

---
+++ src/user/outbound-adapter/post-users.repository.ts
@@ -0,0 +1,32 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../../entities/user.entities';
+import { Repository } from 'typeorm';
+import {
+  PostUsersOutboundPort,
+  PostUsersOutboundPortInputDto,
+  PostUsersOutboundPortOutputDto,
+} from '../outbound-port/post-users.outbout-port';
+import { UnauthorizedException } from '@nestjs/common';
+
+export class PostUsersRepository implements PostUsersOutboundPort {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+  async execute(
+    params: PostUsersOutboundPortInputDto,
+  ): Promise<PostUsersOutboundPortOutputDto> {
+    const findUser = await this.usersRepository.findOne({
+      where: { email: params.email },
+    });
+    if (findUser) {
+      throw new UnauthorizedException('중복 이메일');
+    }
+    const user = await this.usersRepository.save(params);
+    return user.map((users: User) => {
+      return {
+        email: users.email,
+      };
+    });
+  }
+}
