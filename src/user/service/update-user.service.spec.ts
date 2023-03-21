c0d9e9b8529393c73bb00ca6d764e0f17833d3c3
nuobasic <nuobasic@naver.com>
2023-03-22 00:13:07 +0900

    test: update user test

---
+++ src/user/service/update-user.service.spec.ts
@@ -0,0 +1,50 @@
+import { Body } from '@nestjs/common';
+import { Role } from '../../entities/user.role';
+import {
+  UpdateUserOutboundPort,
+  UpdateUserOutboundPortInputDtoBody,
+  UpdateUserOutboundPortInputDtoParma,
+  UpdateUserOutboundPortOutputDto,
+} from '../outbound-port/update-user.outbound.port';
+import { UpdateUserService } from './update-user.service';
+
+class MockUpdateUserOutboundPort implements UpdateUserOutboundPort {
+  private readonly result: UpdateUserOutboundPortOutputDto;
+
+  constructor(result: UpdateUserOutboundPortOutputDto) {
+    this.result = result;
+  }
+
+  async excute(
+    params: UpdateUserOutboundPortInputDtoParma,
+    Body: UpdateUserOutboundPortInputDtoBody,
+  ): Promise<UpdateUserOutboundPortOutputDto> {
+    return this.result;
+  }
+}
+
+describe('UpdateUserService Spec', () => {
+  test('유저 정보 변경', async () => {
+    const user = {
+      userId: 1,
+    };
+    const body = {
+      email: 'test@abc.com',
+      name: 'apple1',
+      password: '1234',
+      role: Role.USER,
+    };
+    const body2 = {
+      email: 'test@abc.com',
+      name: 'apple',
+      password: '1234',
+      role: Role.USER,
+    };
+    const updateUserService = await new UpdateUserService(
+      new MockUpdateUserOutboundPort(body2),
+    );
+    const res = await updateUserService.excute(user, body);
+
+    await expect(res.name).toEqual('apple');
+  });
+});
